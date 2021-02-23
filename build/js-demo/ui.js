bcd57606d3ce592d6cc38d534feb0cf923d3e26d
Илья Долженко <ilay00@mail.ru>
2021-02-24 00:33:24 +0300

    demo fixes

--- build/js-demo/ui.js
+++ build/js-demo/ui.js
@@ -1,90 +1,105 @@
-import {connectMetamask, ensureWalletConnected, requestAccount, uploadFile, 
-  downloadFile, getFiles} from './index.js'
-import {settings, settingsPromise} from './api.js'
-import {getAccount} from './eth.js'
+import {
+    connectMetamask,
+    ensureWalletConnected,
+    requestAccount,
+    uploadFile,
+    downloadFile,
+    getFiles
+} from "./index.js";
+import { settings, settingsPromise } from "./api.js";
+import { getAccount } from "./eth.js";
 
-const $ = document.querySelector.bind(document)
+const $ = document.querySelector.bind(document);
 
-function esc(text){
-  const textnode = document.createTextNode(text)
-  const el = document.createElement('p')
-  el.appendChild(textnode)
-  return el.innerHTML
+function esc(text) {
+    const textnode = document.createTextNode(
+        text.length > 30 ? text.slice(0, 10) + "..." + text.slice(-10) : text
+    );
+    const el = document.createElement("p");
+    el.appendChild(textnode);
+    return el.innerHTML;
 }
 
-function initUploadArea(){
-  $('#drop-area').addEventListener('drop', onFileDrop)
-  $('#drop-area').addEventListener('dragover', onFileDragOver) 
-  $('#file-input').addEventListener('click', async e => {
-    try {
-      await ensureWalletConnected()
-    } catch(ex) {
-      // prevent open file dialog if no metamask
-      e.preventDefault()
-      throw ex
+function initUploadArea() {
+    $("#drop-area").addEventListener("drop", onFileDrop);
+    $("#drop-area").addEventListener("dragover", onFileDragOver);
+    $("#file-input").addEventListener("click", async e => {
+        try {
+            await ensureWalletConnected();
+        } catch (ex) {
+            // prevent open file dialog if no metamask
+            e.preventDefault();
+            throw ex;
+        }
+    });
+    $("#file-input").addEventListener("change", e => {
+        const file = e.target.files[0];
+        upload(file);
+    });
+    // TODO paste event?
+
+    function onFileDragOver(e) {
+        e.preventDefault();
     }
-  })
-  $('#file-input').addEventListener('change', e => {
-    const file = e.target.files[0]
-    upload(file)
-  })
-  // TODO paste event?
 
-  function onFileDragOver(e){
-    e.preventDefault()
-  }
-
-  function onFileDrop(e){
-    e.preventDefault()
-    const file = e.dataTransfer.files[0]
-    if(file == null){
-      return
+    function onFileDrop(e) {
+        e.preventDefault();
+        const file = e.dataTransfer.files[0];
+        if (file == null) {
+            return;
+        }
+        upload(file);
     }
-    upload(file)
-  }
 
-  function onUploadProgress(fraction){
-    const percent = Math.floor(fraction*100) + '%'
-    $('#progress-bar').style = 'width: ' + percent
-    $('#timer').innerText = percent
-  }
-
-  async function upload(file){
-    ensureWalletConnected()
-    uploadFile(file, {onUploadProgress})
-  }
-}
-
-export async function initWalletUI(){
-  $('[name="select-wallet"][value="MetaMask"]').addEventListener('change', async () => {
-    try {
-      const acc = await requestAccount()
-      if(PAGE_STATE != 'demo'){
-        window.location.replace('/demo')
-        return
-      }
-      await connectMetamask(acc)
-      Modal.hideModal()
-    } finally {
-      document.querySelectorAll(['[name=select-wallet]']).forEach(el => 
-        el.checked = false
-      )
+    function onUploadProgress(fraction) {
+        const percent = Math.floor(fraction * 100) + "%";
+        $("#progress-bar").style = "width: " + percent;
+        $("#timer").innerText = percent;
     }
-  })
+
+    async function upload(file) {
+        ensureWalletConnected();
+        uploadFile(file, { onUploadProgress });
+    }
 }
 
-export async function initDemoUI(){
-  initUploadArea()
+export async function initWalletUI() {
+    $('[name="select-wallet"][value="MetaMask"]').addEventListener(
+        "change",
+        async () => {
+            try {
+                const acc = await requestAccount();
+                if (PAGE_STATE != "demo") {
+                    window.location.replace("/demo");
+                    return;
+                }
+                await connectMetamask(acc);
+                Modal.hideModal();
+            } finally {
+                document
+                    .querySelectorAll(["[name=select-wallet]"])
+                    .forEach(el => (el.checked = false));
+            }
+        }
+    );
 }
 
-function formatSize(size){
-  return new BigNumber(size).shiftedBy(-9)/*GB*/.toFixed(3,BigNumber.ROUND_UP) + ' Gb'
+export async function initDemoUI() {
+    initUploadArea();
 }
 
-export async function renderFiles(files){
-  // TODO spinner
-  function row(file) {
-    return `
+function formatSize(size) {
+    return (
+        new BigNumber(size)
+            .shiftedBy(-9) /*GB*/
+            .toFixed(3, BigNumber.ROUND_UP) + " Gb"
+    );
+}
+
+export async function renderFiles(files) {
+    // TODO spinner
+    function row(file) {
+        return `
 			<tr>
 				<td aria-label="ID">
 					<div class="demo-table__btns">
@@ -108,78 +123,81 @@ export async function renderFiles(files){
 					${formatSize(file.size)}
 				</td>
 			</tr>
-    `
-  }
-  let html = ''
-  for(const file of files){
-    html += row(file)
-  }
-  $('#files').innerHTML = html
-  $('#files').addEventListener('click', e => {
-    const id = e.target.dataset.id
-    if(id != null){
-      showFileDetails(id)
+    `;
     }
-  })
-}
-
-function getPrice(size){
-  const PRICE_PER_MONTH_PER_TB = 2.4
-  const DECIMAL_POINTS = 7
-  const str = new BigNumber(size)
-    .times(PRICE_PER_MONTH_PER_TB)
-    .div(new BigNumber(10).pow(12))
-    .toFixed(7,BigNumber.ROUND_UP)
-  return '$' + str
-}
-
-function showFileDetails(id){
-  const file = getFiles().find(file => file.id == id)
-  $('#file-details-id').innerText = file.id
-  $('#file-details-name').innerText = file.filename
-  $('#file-details-size').innerText = formatSize(file.size)
-  $('#file-details-creation-time').innerText = file.creation_time
-  $('#file-details-price-per-month').innerText = getPrice(file.size)
-  $('#file-details-owner').innerText = file.owner
-  $('#file-details-txhash').innerText = file.txhash
-  $('#file-details-txhash').href = 'https://rinkeby.etherscan.io/tx/' + file.txhash
-
-  const isOwner = (getAccount() != null) && 
-    (getAccount().toLowerCase() == file.owner.toLowerCase())
-
-  $('#not_an_owner').style.display = isOwner ? 'none' : 'block'
-  
-  $('#download').disabled = !isOwner
-
-  $('#download').onclick = async e => {
-    e.preventDefault()
-    ensureWalletConnected()
-    if(isOwner){
-      Modal.modalShow('waiting', 'Waiting for confirmation')
-      await downloadFile(id)
-      Modal.hideModal()
-    } else {
-      Modal.modalShow('failed', 'You are not an owner of this file')
+    let html = "";
+    for (const file of files) {
+        html += row(file);
     }
-  }
-  Modal.modalShow('file-details')
+    $("#files").innerHTML = html;
+    $("#files").addEventListener("click", e => {
+        const id = e.target.dataset.id;
+        if (id != null) {
+            showFileDetails(id);
+        }
+    });
 }
 
-export function renderAccount(account){
-  const ACC_LENGTH = 42
-  //format like 0x0659...71FA
-  const acc_str = account.slice(0, 6) + '...' + account.slice(ACC_LENGTH - 4, ACC_LENGTH)
-  $('#account').innerText = acc_str
+function getPrice(size) {
+    const PRICE_PER_MONTH_PER_TB = 2.4;
+    const DECIMAL_POINTS = 7;
+    const str = new BigNumber(size)
+        .times(PRICE_PER_MONTH_PER_TB)
+        .div(new BigNumber(10).pow(12))
+        .toFixed(7, BigNumber.ROUND_UP);
+    return "$" + str;
 }
 
-export function renderBalance(balance){
-  $('#header').classList.add('in-logged')
-  $('#header_logged_in').style.display = 'block'
-  $('#header_logged_out').style.display = 'none'
-  settingsPromise.then(() => {
-    const balance_str = balance
-      .shiftedBy(-settings.cls_token_decimals)
-      .toFixed(5,BigNumber.ROUND_DOWN)
-    $('#balance').innerText = balance_str
-  })
+function showFileDetails(id) {
+    const file = getFiles().find(file => file.id == id);
+    $("#file-details-id").innerText = file.id;
+    $("#file-details-name").innerText = file.filename;
+    $("#file-details-size").innerText = formatSize(file.size);
+    $("#file-details-creation-time").innerText = file.creation_time;
+    $("#file-details-price-per-month").innerText = getPrice(file.size);
+    $("#file-details-owner").innerText = file.owner;
+    $("#file-details-txhash").innerText = file.txhash;
+    $("#file-details-txhash").href =
+        "https://rinkeby.etherscan.io/tx/" + file.txhash;
+
+    const isOwner =
+        getAccount() != null &&
+        getAccount().toLowerCase() == file.owner.toLowerCase();
+
+    $("#not_an_owner").style.display = isOwner ? "none" : "block";
+
+    $("#download").disabled = !isOwner;
+
+    $("#download").onclick = async e => {
+        e.preventDefault();
+        ensureWalletConnected();
+        if (isOwner) {
+            Modal.modalShow("waiting", "Waiting for confirmation");
+            await downloadFile(id);
+            Modal.hideModal();
+        } else {
+            Modal.modalShow("failed", "You are not an owner of this file");
+        }
+    };
+    Modal.modalShow("file-details");
+}
+
+export function renderAccount(account) {
+    const ACC_LENGTH = 42;
+    //format like 0x0659...71FA
+    const acc_str =
+        account.slice(0, 6) + "..." + account.slice(ACC_LENGTH - 4, ACC_LENGTH);
+    $("#account").innerText = acc_str;
+}
+
+export function renderBalance(balance) {
+    $("#header").classList.add("in-logged");
+    $("#header_logged_in").style.display = "block";
+    $("#header_logged_out").style.display = "none";
+    settingsPromise.then(() => {
+        const balance_str = balance
+            .shiftedBy(-settings.cls_token_decimals)
+            .toFixed(5, BigNumber.ROUND_DOWN);
+        $("#balance").innerText = balance_str;
+    });
 }
